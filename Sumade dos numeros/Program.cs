e0ac5c378a1cdc979d290cb2be68949bb82b5eb5
ForánDavidDurango1725 <forand.d305@gmail.com>
2023-09-04 23:24:24 -0500

    Agregar archivos de proyecto.

---
+++ Sumade dos numeros/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sumade_dos_numeros
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }using System;
+
+class SumaDosNumeros
+    {
+        static void Main()
+        {
+            int num1, num2, suma;
+
+            Console.Write("Ingrese el primer número: ");
+            num1 = int.Parse(Console.ReadLine());
+            Console.Write("Ingrese el segundo número: ");
+            num2 = int.Parse(Console.ReadLine());
+
+            suma = num1 + num2;
+
+            Console.WriteLine("La suma es: " + suma);
+        }
+    }
+}
+}
